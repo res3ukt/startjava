455418f4ee6f44c3406f066f9cddd270b72c8adf
Alex <res3ukt@tutanota.com>
2019-06-21 20:05:35 +0300

    HW

---
+++ lesson_1/Variable.java
@@ -0,0 +1,27 @@
+public class Variable {
+  public static void main (String [] args){
+    
+    int core = 4;
+    double frequency = 1.7;
+    byte systemType = 64;
+    boolean cdDvd = true;
+    long hdd = 500;
+    float ram = 8.0f;
+    char screen = 96;
+    double diagonalScreen = 15.5;
+    short video = 1;
+
+    System.out.println("core: " + core  +  ",  frequency core: " + frequency + 
+      ", systemType: " + systemType + ", cdDvd: " + cdDvd + ", hdd: " + hdd + 
+       ", ram: " + ram + ", video: " + video + ", screen: " + diagonalScreen + screen);
+
+
+
+
+
+
+
+
+
+  }
+}
